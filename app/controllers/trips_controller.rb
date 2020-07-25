f637a5690ee84958ce6430da87f404c3c7e6f236
Arturo Hernandez <marksandobal@gmail.com>
2020-07-24 21:02:43 -0500

    Add list to Trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,6 @@
 class TripsController < ApplicationController
   def index
+    @trips = Trip.all
+    @trip_per_days = @trips.group_by{ |trip| trip.date.strftime("%d %B") }
   end
 end
--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,2 +1,13 @@
 module TripsHelper
+  def first_stop
+    @trip_stops.first
+  end
+
+  def last_stop
+    @trip_stops.last
+  end
+
+  def trip_stops(stops)
+    @trip_stops = stops.order(created_at: :asc)
+  end
 end
